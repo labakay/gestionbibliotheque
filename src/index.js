069e5f480fd8eff0b5fd3e899060f6922637ecbe
ismaila <lodia.ismaila@gmail.com>
2021-01-21 11:15:23 +0100

    register bis

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    App / >
+    <
+    /React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
